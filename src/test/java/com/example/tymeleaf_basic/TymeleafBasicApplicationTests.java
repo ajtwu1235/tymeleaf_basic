35347a98d23d8079335b5adf3e20844d4e5576c8
ajtwu1235 <75232182+ajtwu1235@users.noreply.github.com>
2022-05-15 18:00:02 +0900

    Initial commit

---
+++ src/test/java/com/example/tymeleaf_basic/TymeleafBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tymeleaf_basic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TymeleafBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
